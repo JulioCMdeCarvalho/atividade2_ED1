bd673a19d1474d1fe6a94e9f8a77008b10fe8cc9
Julio Cesar Maia de Carvalho <jcmc94@hotmail.com>
2020-10-21 15:39:44 -0300

    Add files via upload

--- AAA_ATIVIDADE2_PONTEIROS/questao1_atividade2_listaPonteiros/main.c
+++ AAA_ATIVIDADE2_PONTEIROS/questao1_atividade2_listaPonteiros/main.c
@@ -1,5 +1,4 @@
 /*
-
 1. Considere o trecho de programa a seguir:
 
 int i = 3, j = 5;
@@ -28,7 +27,6 @@ q) **&p + **&q;
 r) ++*q / *p * ++j;
 s) *q / *p * (float)++i;
 t) 2 * *q / --*p * ++i;
-
 */
 
 #include <stdio.h>
@@ -41,60 +39,69 @@ int i = 3, j = 5;
     p = &i;
     q = &j;
 
-    int letra_a;
-    letra_a = *p;
-
     printf("\n Saidas: \n");
     //1 para TRUE
     //o para FALSE
+
     //letra a
-    printf("\n a: %d \n", letra_a);
+    printf("\n a: %d \n", *p);
+
     //letra b
     printf("\n b: %d \n", *q*i);
+
     //letra c
-    int resul;
-    resul = (p == &i); //var resul nao precisa
-    printf("\n c: %d \n", resul);
+    printf("\n c1: %d \n", (p == &i));
 
     //letra d
-    printf("\n d: %d \n", q < &j );
+    printf("\n d1: %d \n", q < &j );
+
     //letra e
-    printf("\n e: %d\n", (*p > i) && !(j < *q));
+    printf("\n e1: %d\n", (*p > i) && !(j < *q) );
+
     //letra f
-    printf("\n f: %d\n", !(*p == i) || (j < *q));
+    printf("\n f1: %d \n", !(*p == i) || (j < *q));
+
     //letra g
-    printf("\n g: %d\n", *p - *q);
+    printf("\n g0: %d \n", *p - *q);
+
     //letra h
-    printf("\n h: %d\n", i / *q);
+    printf("\n h1: %d\n", i / *q);
+
     //letra i
     printf("\n i: %d\n", *p * *q);
+
     //letra j
     printf("\n j: %d\n", (float)*q / *p);
+
     //letra k
     printf("\n k: %d\n", 5.0 * *q / *p - 2);
-    //letra l
-    printf("\n l: %d\n", *q / *p * 5.0 - 2);
 
-    //int resul3;
-    //resul3 = *q / (*p – 1.0);
-    //printf("\n m: %d\n", *q / (*p – 1.0));
-    //printf("\n m: %d \n", resul3);
+    //letra l
+    printf("\n l: %.2f \n", *q / *p * 5.0 - 2);
 
     //letra m
+    //printf("\n m: %d\n", *q / (*p – 1.0));
     printf("\n m: invalido \n");
+
     //letra n
-    printf("\n n: %d\n", 3 * - *p / *q + 7);
+    printf("\n n1: %d\n", 3 * - *p / *q + 7);
+
     //letra o
-    printf("\n o: %d\n", ++*q / *p);
+    printf("\n o2: %d\n", ++*q / *p);
+
     //letra p
-    printf("\n p: %d\n", *p / (float)++*q);
+    printf("\n p1: %d\n", *p / (float)++*q);
+
     //letra q
-    printf("\n q: %d\n", **&p + **&q);
+    printf("\n q: %d \n", **&p + **&q);
+
     //letra r
-    printf("\n r: %d\n", ++*q / *p * ++j);
+    printf("\n r: %d \n", ++*q / *p * ++j);
+
     //letra s
-    printf("\n s: %d\n", *q / *p * (float)++i);
+    printf("\n s: %d \n", *q / *p * (float)++i);
+
     //letra t
-    printf("\n t: %d\n", 2 * *q / --*p * ++i);
+    printf("\n t: %d \n", 2 * *q / --*p * ++i);
 
 }
