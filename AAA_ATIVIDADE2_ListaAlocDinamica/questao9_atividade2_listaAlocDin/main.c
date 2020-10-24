26825664a051c8e43039170867cb7d22740f4caa
Julio Cesar Maia de Carvalho <jcmc94@hotmail.com>
2020-10-24 19:39:35 -0300

    Add files via upload

--- AAA_ATIVIDADE2_ListaAlocDinamica/questao9_atividade2_listaAlocDin/main.c
+++ AAA_ATIVIDADE2_ListaAlocDinamica/questao9_atividade2_listaAlocDin/main.c
@@ -1,7 +1,6 @@
 /*
 9. Considere um cadastro de produtos de um estoque,
 com as seguintes informações para cada produto:
-
 • Código de identificação do produto: representado por um valor inteiro
 • Nome do produto: com ate 50 caracteres
 • Quantidade disponível no estoque: representado por um número inteiro
@@ -22,31 +21,171 @@ e imprima na tela.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <locale.h>
 
-struct produto{
+/*
+a-ok
+b-
+c-
+d-
+
+*/
+
+typedef struct produto{
     int codigo_id_produto, qtd_disp_estoque;
     char nome[50];
     float preco_de_venda;
-};
+}Produto;
 
-typedef struct produto PRODUTO;
+
 
 int main()
 {
-    setlocale(LC_ALL,"");
-
-    int n, i;
-    int maiorPreco=0, maiorQTDestoque=0;
-
-    PRODUTO **ponteiroProduto;
+    int n, i, maiorQTDestoque = 0;
+    float maiorPreco=0.0;
 
     printf("Digite a quantidade de produtos a serem cadastrados: \n");
     scanf("%d", &n);
-    ponteiroProduto = malloc(n*sizeof(PRODUTO*));
+
+    //Produto prod[n]; //vetor prod de n posicoes do tipo Produto
+
+    Produto *p = malloc(n*sizeof(Produto));
+
+    //int qtd;
+    //scanf("%d", &qtd);
+    // CARRO *carros = malloc(qtd * sizeof(struct CARRO));
+    //free(carros); //só para usar a variável, neste caso não precisa
+
     for(i=0;i<n;i++){
-        ponteiroProduto[i] = malloc(sizeof(PRODUTO));
+        printf("\n Entre com o Codigo do Produto: ");
+        //scanf("%d", &p->codigo_id_produto);
+        scanf("%d", &p[i].codigo_id_produto);
+        printf("\n Entre com a quantidade em Estoque do Produto: ");
+        //scanf("%d", &p->qtd_disp_estoque);
+        scanf("%d", &p[i].qtd_disp_estoque);
+        printf("\n Entre com o nome do produto: ");
+        //scanf("%s", p->nome);
+        scanf("%s", p[i].nome);
+        printf("\n Preço: ");
+        //scanf("%f", &p->preco_de_venda);
+        scanf("%f", &p[i].preco_de_venda);
     }
+
+    //letra c
+    //define qual produto com o maior preco
+    for (i=0;i<n;i++) {
+        if(p[i].preco_de_venda > maiorPreco) {
+            maiorPreco = p[i].preco_de_venda;
+        }
+    }
+    //produto com maior preco
+    for (i=0;i<n;i++) {
+        if(p[i].preco_de_venda == maiorPreco) {
+            printf("\n == Produto com maior preco == \n");
+            printf("Codigo: %d \n", p[i].codigo_id_produto);
+            printf("Nome: %s \n", p[i].nome);
+            printf("Preco: %.2f \n", p[i].preco_de_venda);
+            printf("Quantidade disponivel em estoque: %d \n", p[i].qtd_disp_estoque);
+        }
+    }
+
+    //letra d
+    //define qual produto com o maior quantidade disponivel em estoque
+    for (i=0;i<n;i++) {
+        if(p[i].qtd_disp_estoque > maiorPreco) {
+            maiorQTDestoque = p[i].qtd_disp_estoque;
+        }
+    }
+    //produto com maior preco
+    for (i=0;i<n;i++) {
+        if(p[i].qtd_disp_estoque == maiorQTDestoque) {
+            printf("\n == Produto com maior quantidade disponivel em estoque == \n");
+            printf("Codigo: %d \n", p[i].codigo_id_produto);
+            printf("Nome: %s \n", p[i].nome);
+            printf("Preco: %.2f \n", p[i].preco_de_venda);
+            printf("Quantidade disponivel em estoque: %d \n", p[i].qtd_disp_estoque);
+        }
+    }
+
+    printf("\n ====== DADOS DIGITADOS ====== \n");
+    for(i=0;i<n;i++){
+
+       // printf("\n Produto %d:", i);
+       // printf("\n Codigo: %d", p->codigo_id_produto);
+       // printf("\n Quantidade em estoque: %d", p->qtd_disp_estoque);
+       // printf("\n Nome: %s", p->nome);
+       // printf("\n Preço: %.2f", p->preco_de_venda);
+        printf("\n ================");
+        printf("\n Produto %d:", i);
+        printf("\n Codigo: %d", p[i].codigo_id_produto);
+        printf("\n Quantidade em estoque: %d", p[i].qtd_disp_estoque);
+        printf("\n Nome: %s", p[i].nome);
+        printf("\n Preço: %.2f", p[i].preco_de_venda);
+        printf("\n ================");
+    }
+
+}
+
+/*
+funciona 02
+//cadastra os produtos
+//string nao aceita espaco nao sei porque
+
+
+*/
+
+
+/*
+//funciona 01
+//1 elemento para a estrutura
+
+
+int main()
+{
+    int n, i;
+    int maiorPreco=0, maiorQTDestoque=0;
+
+    Produto *p;
+
+    p = (Produto*)malloc(sizeof(Produto));
+
+    printf("\n Cadastro de Produtos \n");
+    printf("\n Entre com o Codigo do Produto: ");
+    scanf("%d", &p->codigo_id_produto);
+
+    printf("\n Entre com a quantidade em Estoque do Produto: ");
+    scanf("%d", &p->qtd_disp_estoque);
+
+    printf("\n Entre com o nome do produto: ");
+    scanf("%s", p->nome);
+
+    printf("\n Preço: ");
+    scanf("%f", &p->preco_de_venda);
+
+    printf("\n ====== DADOS DIGITADOS ====== \n");
+    printf("\n Codigo: %d", p->codigo_id_produto);
+    printf("\n Quantidade em estoque: %d", p->qtd_disp_estoque);
+    printf("\n Nome: %s", p->nome);
+    printf("\n Preço: %.2f", p->preco_de_venda);
+}
+
+*/
+
+/*
+//antigo
+
+
+    PRODUTO *ponteiroProduto;
+
+    printf("Digite a quantidade de produtos a serem cadastrados: \n");
+    scanf("%d", &n);
+
+    //ponteiroProduto = malloc(n*sizeof(PRODUTO*));
+
+    ponteiroProduto = (PRODUTO*)malloc(n*sizeof(PRODUTO));
+
+    //for(i=0;i<n;i++){
+      //  ponteiroProduto[i] = malloc(sizeof(PRODUTO));
+    //}
 
     //CONSERTAR A DECLARACAO DAS ESTRUTURAS
     //esta invadindo memoria
@@ -58,8 +197,9 @@ int main()
         printf("\n Digite o codigo do produto: \n");
         scanf("%d", ponteiroProduto[i]->codigo_id_produto);
         printf("\n Digite o nome do produto: \n");
-        fflush(stdin);
-        fgets(ponteiroProduto[i]->nome, 50, stdin);
+        //fflush(stdin);
+        //fgets(ponteiroProduto[i]->nome, 50, stdin);
+        scanf("%s", ponteiroProduto[i]->nome)
         printf("\n Digite a quantidade disponivel em estoque: \n");
         scanf("%d", ponteiroProduto[i]->qtd_disp_estoque);  //erro aqui
         printf("\n Digite o Preco em Reais: \n");
@@ -102,4 +242,8 @@ int main()
             printf("Quantidade disponivel em estoque: %d \n", ponteiroProduto[i]->qtd_disp_estoque);
         }
     }
-}
+
+
+
+*/
+
